66185aed525ba86c8ea75d6d6afd85d7682c1c8d
bbbb <bgh9651@gmail.com>
2022-09-25 13:56:26 +0900

    conflict 해결

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,82 +1,34 @@
 /// <reference types="react-scripts" />
-<<<<<<< HEAD
-
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
   }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.avif' {
   const src: string;
   export default src;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.bmp' {
   const src: string;
   export default src;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.gif' {
   const src: string;
   export default src;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.jpg' {
   const src: string;
   export default src;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.jpeg' {
   const src: string;
   export default src;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.png' {
   const src: string;
   export default src;
 }
-<<<<<<< HEAD
-
-declare module '*.webp' {
-    const src: string;
-    export default src;
-}
-
-declare module '*.svg' {
-  import * as React from 'react';
-
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<
-    SVGSVGElement
-  > & { title?: string }>;
-
-  const src: string;
-  export default src;
-}
-
-=======
 declare module '*.webp' {
   const src: string;
   export default src;
@@ -89,28 +41,15 @@ declare module '*.svg' {
   const src: string;
   export default src;
 }
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
 declare module '*.module.scss' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-<<<<<<< HEAD
-
 declare module '*.module.sass' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-=======
-declare module '*.module.sass' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
->>>>>>> d4a126d (Added weather icons and interfaces for the overlays)
